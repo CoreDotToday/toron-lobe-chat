f53d377f4d2ea277815f52fe1b3ac52f14b49c8d
jourmooney <jourmooney@gmail.com>
2023-12-22 17:16:41 +0900

    🐛 fix: Build error

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-// const handleClick = async () => {
-//   await fetch('/api/user', {
-//     method: 'POST',
-//   });
-// };
+const handleClick = async () => {
+  await fetch('/api/user', {
+    method: 'POST',
+  });
+};
 
 export default function Test() {
   return (
-    <div>버튼</div>
-    // <button type="button" onClick={handleClick}>
-    //   버튼
-    // </button>
+    <button type="button" onClick={handleClick}>
+      버튼
+    </button>
   );
 }
